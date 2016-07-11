45e3991ad5659195c9b843815bec33c9d3b5af63
James Didehvar <james@didehvar.com>
2016-07-11 20:08:06 +0100

    eslint require semi colons

--- config/index.js
+++ config/index.js
@@ -19,6 +19,14 @@ module.exports = {
   dev: {
     env: require('./dev.env'),
     port: 8080,
-    proxyTable: {}
+    proxyTable: {
+      '/api': {
+        target: 'http://localhost:3000',
+        changeOrigin: true,
+        pathRewrite: {
+          '^/api': ''
+        }
+      }
+    }
   }
 }
