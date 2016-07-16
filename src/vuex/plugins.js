b8a59a847ee884b423ca75ba3f5e7991900ef94f
James Didehvar <james@didehvar.com>
2016-07-16 11:40:34 +0100

    remove unused code

--- src/vuex/plugins.js
+++ src/vuex/plugins.js
@@ -1,27 +1,10 @@
 import createLogger from 'vuex/logger';
-// import Vue from 'vue';
-// import { AUTH_START } from './mutation-types';
 
 const localStoragePlugin = store => {
   store.subscribe((mutation, state) => localStorage.setItem('state', JSON.stringify(state)));
 };
 
-// const authHeaders = store => {
-//   store.subscribe((mutation, state) => {
-//     if (mutation.type !== AUTH_START) return;
-//
-//     const { token, clientId, userId, expiry } = state.auth.auth;
-//
-//     Vue.http.headers.common['token-type'] = 'Bearer';
-//     Vue.http.headers.common['access-token'] = token;
-//     Vue.http.headers.common.client = clientId;
-//     Vue.http.headers.common.uid = userId;
-//     Vue.http.headers.common.expiry = expiry;
-//   });
-// };
-
 export default [
   createLogger(),
   localStoragePlugin
-  // authHeaders
 ];
