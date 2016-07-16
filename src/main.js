9d85299121afb4238119c838ed15e50085a263bf
James Didehvar <james@didehvar.com>
2016-07-16 11:29:01 +0100

    remove duplicate code

--- src/main.js
+++ src/main.js
@@ -5,27 +5,12 @@ import App from './App';
 import Login from './components/Login';
 import Display from './components/Display';
 import Increment from './components/Increment';
-import store from './vuex/store';
 import { api } from './config';
-import { AUTH_STORE } from './vuex/mutation-types';
 
 Vue.use(VueRouter);
 Vue.use(VueResource);
 
 Vue.http.options.root = api.base;
-
-Vue.http.interceptors.push((request, next) => {
-  next(response => {
-    if (!response.headers['access-token']) return;
-
-    store.dispatch(AUTH_STORE, {
-      token: response.headers['access-token'],
-      clientId: response.headers.client,
-      userId: response.headers.uid,
-      expiry: response.headers.expiry
-    });
-  });
-});
 
 const router = new VueRouter({
   history: true
